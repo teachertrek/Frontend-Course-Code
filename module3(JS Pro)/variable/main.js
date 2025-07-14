447d93c733eed744e51b94abc0317e21ccab577e
Your Name <you@example.com>
2025-07-14 22:11:24 +0530

    variable delivered in js

---
+++ module3(JS Pro)/variable/main.js
@@ -0,0 +1,34 @@
+console.log('Hello')
+
+var age=25;
+var name="Rahul";
+let city="Delhi";
+const state="Delhi";
+
+console.log(typeof age)
+console.log(typeof name)
+
+var marks=80;
+var marks=70;
+
+marks=60;
+marks="seventy";
+marks=[12, 34, 56]
+console.log(marks)
+
+let degree="B.tech";
+degree=60;
+degree="seventy";
+degree=[12, 34, 56]
+console.log(degree)
+// let degree="12th";
+
+const semester="seventh";
+// semester="first";
+
+// const subject;
+
+
+
+
+
