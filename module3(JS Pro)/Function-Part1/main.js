401ad11c0a068319b011a836769e817b6d762c24
Your Name <you@example.com>
2025-07-21 21:30:33 +0530

    string and function delivered

---
+++ module3(JS Pro)/Function-Part1/main.js
@@ -0,0 +1,24 @@
+console.log("Function Part1");
+
+function addTwoNum(num1, num2, num3=6){
+    
+    let res=num1+num2+num3;
+   
+    return res;
+}
+let sum=addTwoNum(2,4,7);
+console.log(sum)
+
+// console.log(addTwoNum(5,7))
+
+
+// function add(){
+//     let num1=3;
+//     let num2=2;
+//     let res=num1+num2;
+//     console.log(res)
+// }
+// add()
+
+
+
---
+++ module3(JS Pro)/string/main.js
@@ -0,0 +1,61 @@
+console.log("String")
+
+// let web="Teacher Trek"
+// let str = "Helpo";
+// console.log(str[3])
+// str[0] = "J"; 
+
+
+
+// console.log(web.charAt(2))
+// Syntax:
+// let length = str.length;
+
+// string, number, symbol
+
+// let str = "Hello World1@";
+// console.log(str.length); // Output: 5
+
+// Syntax:
+// let slicedStr = str.slice(startIndex, endIndex);
+
+// let str = "Hello World Welcome ";
+
+// console.log(str.slice(0, 5));     // Output: Hello
+// console.log(str.slice(6));        // Output: World
+// console.log(str.slice(-5));       // Output: World
+// console.log(str.slice(-11, -6));  // Output: Hello
+
+// let str = "Hello World";
+// console.log(str.substring(-3, -5)); // Output: Hello
+// console.log(str.substring(6)); // Output: World
+
+// let str1 = "Hello";
+// let whiteSpace="TT"
+// let str2 = "World";
+// console.log(str1.concat(whiteSpace, str2)); // Output: Hello World
+
+// let str = "Hello Woeld";
+// console.log(str.indexOf("r")); // Output: 6
+// console.log(str.indexOf("e")); // Output: -1
+
+// Syntax:
+// let trimmedStr = str.trim();
+
+// let str = "  Hello World ";
+// console.log(str.trim().length); // Output: "Hello"
+
+// let tt="Like_Share_And Subscruibe";
+// console.log(tt.split("").length)
+let city=`Delhi`;
+
+console.log(city)
+let name = "Prakash";
+let greeting = `Hello, ${name}!`;
+console.log(greeting); // Output: Hello, Prakash!
+
+
+const address='delhi, "delhi", 576757'
+
+
+
