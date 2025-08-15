293ce113b386f1433d4fc03b0463ee7cedf080b3
Your Name <you@example.com>
2025-08-15 11:23:56 +0530

    done

---
+++ module3(JS Pro)/hoisting/main.js
@@ -0,0 +1,69 @@
+// console.log(city);
+// var city = "Delhi";
+
+// function test() {
+
+//   console.log(age);
+//   var age = 10;
+//   console.log(age);
+// }
+// test();
+
+// function parent() {
+//   var num = 1;
+
+//   function child() {
+    
+
+
+//     // console.log(num)
+//     // var num = 1;
+//     console.log(num)
+//   }
+//   child();
+// }
+// parent();
+
+// {
+// console.log(city)
+// let city;
+// console.log(city)
+// }
+
+// var errorval;
+// errorval=undefined;
+// errorval="This will be hoisted"
+// console.log(undefined)
+// try {
+//     console.log(errorVar); 
+//     var errorVar = 'This will be hoisted';
+//     throw new Error('Test error');
+// } catch (error) {
+//     console.log(errorVar); 
+// }
+
+// console.log(test(1,5))
+// function test(c,d){
+   
+//     return c+d
+// }
+// parent()
+// function parent(){
+//     // console.log(child)
+//     child()
+
+//     function child(){
+//         console.log("I am child")
+//     }
+// }
+console.log(printMyName)
+// printMyName()
+let printMyName=()=>{
+    console.log("Prakash")
+}
+
+
+
+
+
+
